a386362e4ab0160b4757aec49fbf4603be726462
Timothy Coish <tdcoish@gmail.com>
2020-12-21 17:11:55 -0800

    Art assets added + pc controllable.
    Finally the fun can begin.

---
+++ 2D_Halo_Style_Shooter/Assets/Art/Sprites/readme.txt
@@ -0,0 +1,5 @@
+This folder exists so you can push raw art assets into the project. I can simply drag these assets into the Unity Assets folder, where they can then be used in the project.
+
+Second change.
+
+Third change.
---
+++ 2D_Halo_Style_Shooter/Assets/Scripts/Player/PC_Cont.cs
@@ -0,0 +1,80 @@
+﻿/*************************************************************************************
+
+*************************************************************************************/
+using UnityEngine;
+
+public class PC_Cont : MonoBehaviour
+{
+
+    private Rigidbody2D                     cRigid;
+    // private PC_Gun                          cGun;
+    // private PC_Grnd                         cGrnd;
+    private PC_Shields                      cShields;
+
+    public float                            _spd;
+    public float                            _maxHealth = 1000f;
+    public float                            _health;
+    
+    public UI_PC                            rUI;
+
+    public bool                             _invinsible = false;
+
+    void Start()
+    {
+        cRigid = GetComponent<Rigidbody2D>(); 
+        // cGun = GetComponent<PC_Gun>();   
+        // cGrnd = GetComponent<PC_Grnd>();
+        cShields = GetComponent<PC_Shields>();  
+
+        rUI = FindObjectOfType<UI_PC>();
+        if(rUI == null){
+            Debug.Log("No PC User Interface Found");
+        }
+
+        _health = _maxHealth;
+    }
+
+    void Update()
+    {
+        cRigid.velocity = HandleInputForVel();
+        RotateToMouse();
+
+        // cGun.FRun();
+        // cGrnd.FRun();
+        // CheckDead();
+        // rUI.FSetBarSize(_health/_maxHealth);
+        // rUI.FSetAmmoBarSize(cGun._ammo, cGun._maxAmmo);
+        // rUI.FSetShieldBarSize(cShields._val, cShields._maxVal);
+        // rUI.FSetTimeText(Time.time);
+    }
+
+    private Vector3 HandleInputForVel()
+    {
+        Vector2 vVel = new Vector2();
+        if(Input.GetKey(KeyCode.A)){
+            vVel.x -= _spd;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            vVel.x += _spd;
+        }
+        if(Input.GetKey(KeyCode.W)){
+            vVel.y += _spd;
+        }
+        if(Input.GetKey(KeyCode.S)){
+            vVel.y -= _spd;
+        }
+        
+        vVel = Vector3.Normalize(vVel) * _spd;
+        return vVel;
+    }
+
+    private void RotateToMouse(){
+		Camera c = Camera.main;
+		Vector2 msPos = c.ScreenToWorldPoint(Input.mousePosition);
+
+		Vector2 distance = msPos - (Vector2)transform.position;
+		float angle = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg;
+		angle -= 90;
+		transform.eulerAngles = new Vector3(0, 0, angle);
+	}
+}
